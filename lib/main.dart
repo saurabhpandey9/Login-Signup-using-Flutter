bc7d45fa5b637a40e9cb6804320a9850ad1ba55d
Saurabh Pandey <saurabh17100@gmail.com>
2020-05-13 17:43:03 +0530

    SplashScreen

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,11 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        body: Center(
+          child: Container(
+            child: LoginPage(),
+          ),
+        ),
 
       ),
 
@@ -20,6 +25,74 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: <Widget>[
+        Column(
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.all(20),
+              padding: EdgeInsets.all(10),
+
+              child: Text (
+                "Developerdesk9.Inc",
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Container(
+              child: Text(
+                "Welcome to developerdesk9",
+                style: TextStyle(
+                  fontSize: 15
+                ),
+              ),
+            )
+          ],
+        ),
+
+        Container(
+          child: Image.asset("assets/images/lock_icon.png")
+
+        ),
+
+        Container(
+          width: double.infinity,
+          padding: EdgeInsets.all(20),
+          margin: EdgeInsets.symmetric(
+              horizontal: 30
+          ),
+          decoration: BoxDecoration(
+            color: Colors.black,
+
+            borderRadius: BorderRadius.circular(30),
+          ),
+          child: Center(
+            child: Text(
+              "Get Started",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 15,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+
+        ),
+      ],
+    );
+  }
+}
 
 
 
